0d912dbb1da5283f07826627d984b5e3b516f669
Svetlana A. Pivovarova <pivovarova.svetlana@gmail.com>
2022-11-09 09:33:34 +0400

    html, cscc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./scss/app.scss";
+
+import Header from "./components/Header";
+import Sort from "./components/Sort";
+import Categories from "./components/Categories";
+import PizzaItem from "./components/PizzaItem";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaItem title="Peppironi" price={550} />
+            <PizzaItem title="Cheese" price={400} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
