fa02996f2fea7d14209fc9208ea17ffa2147d1b2
svetlana <pivovarova.svetlana@gmail.com>
2022-12-22 11:35:41 +0400

    feat: add sceletons

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ import Sceleton from "./components/PizzaItem/Sceleton";
 
 function App() {
   const [pizzaItems, setPizzaItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://6375e612b5f0e1eb85fc74a8.mockapi.io/api/items")
       .then((res) => res.json())
       .then((json) => {
         setPizzaItems(json);
+        setIsLoading(false);
       });
   }, []);
 
@@ -30,17 +32,19 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzaItems.map((obj) => (
-              <Sceleton
-                key={obj.id}
-                {...obj}
-                //title={obj.name}
-                //price={obj.price}
-                //image={obj.imageUrl}
-                //sizes={obj.sizes}
-                //types={obj.types}
-              />
-            ))}
+            {isLoading
+              ? [...new Array(6)].map((_, index) => <Sceleton key={index} />)
+              : pizzaItems.map((obj) => (
+                  <PizzaItem
+                    key={obj.id}
+                    {...obj}
+                    //title={obj.name}
+                    //price={obj.price}
+                    //image={obj.imageUrl}
+                    //sizes={obj.sizes}
+                    //types={obj.types}
+                  />
+                ))}
           </div>
         </div>
       </div>
